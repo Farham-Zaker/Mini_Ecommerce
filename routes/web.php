94b892af7e333ed1e73a2952c6171d809f691a49
Farham <farham.zaker007@gmail.com>
2025-07-14 23:54:23 +0330

    refactor(product): update sub route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("product")->group(function () {
+Route::prefix("products")->group(function () {
     Route::get("/", [ProductController::class, "index"]);
 });
