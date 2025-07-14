f92b1cd246670730281909ff7d528637dea4d6eb
Farham <farham.zaker007@gmail.com>
2025-07-14 23:53:31 +0330

    feat(product): add controller for index product list

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("product")->group(function () {
-    Route::get("/", );
+    Route::get("/", [ProductController::class, "index"]);
 });
