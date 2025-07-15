864480a6cd85ffbfd8f205d382a7d8cbd92b1b79
Farham <farham.zaker007@gmail.com>
2025-07-15 09:12:22 +0330

    feat: add 404 template

---
+++ resources/views/404.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <title>صفحه مورد نظر یافت نشد</title>
+    @vite(["resources/css/404.css"])
+</head>
+<body>
+    <h1>404</h1>
+    <h2>صفحه‌ای که به دنبال آن هستید پیدا نشد!</h2>
+    <a href="{{ url('/products') }}">بازگشت به صفحه محصولات</a>
+</body>
+</html>
