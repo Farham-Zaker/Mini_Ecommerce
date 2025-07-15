0de2df32d96deb986a834ae9c2338c19beb8632d
Farham <farham.zaker007@gmail.com>
2025-07-15 09:15:39 +0330

    feat(product): add controller for '/confirm' route

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,16 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        
+
         return view("products", ["products" => $products]);
+    }
+    public function confirm(Request $request){
+        $product_id = $request->input('id');
+        
+        $product = Product::find($product_id);
+
+        if(!$product) return view("404");
+
+        return view("confirm", ["product" => $product]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix("products")->group(function () {
     Route::get("/", [ProductController::class, "index"])->name("product.index");
-    Route::get("/confirm")->name("confirm");
+    Route::get("/confirm", [ProductController::class, "confirm"])->name("confirm");
 });
