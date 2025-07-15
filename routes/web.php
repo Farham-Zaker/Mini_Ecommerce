86a61e278afde4a59215fe22686c3093593f1ea9
Farham <farham.zaker007@gmail.com>
2025-07-15 08:57:39 +0330

    feat(product): set name for '/' route

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("products")->group(function () {
-    Route::get("/", [ProductController::class, "index"]);
+    Route::get("/", [ProductController::class, "index"])->name("product.index");
 });
