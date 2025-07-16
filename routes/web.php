e74c10a8bcac22475e2091664b11a2e6ae342c68
Farham <farham.zaker007@gmail.com>
2025-07-16 09:41:33 +0330

    feat(payment): add routes '/pay' & '/payment/confirm' route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,6 @@ Route::prefix("products")->group(function () {
     Route::get("/", [ProductController::class, "index"])->name("product.index");
     Route::get("/confirm", [ProductController::class, "confirm"])->name("confirm");
 });
+
+Route::get("/pay",[PaymentController::class,"pay"])->name("pay");
+Route::get("/payment/confirm")->name("payment.confirm");
